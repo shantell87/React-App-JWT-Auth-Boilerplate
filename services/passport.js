9b7f14538727596a97f69e33abb070f582265c5f
Shantell Davis <shantell87@gmail.com>
2019-07-26 12:58:49 -0700

    configure jwt strategy

--- services/passport.js
+++ services/passport.js
@@ -12,13 +12,13 @@ const localOptions = { usernameField: 'email' };
 
 const localLogin = new LocalStrategy(localOptions, async (email, password, done) => {
     try {
-        const user = await User.findOne({email});
-        if(!user){
+        const user = await User.findOne({ email });
+        if (!user) {
             return done(null, false);
         }
-        user.comparePassword(password, (err, isMatch) =>{
-            if(err) return done(err);
-            if(!isMatch) {
+        user.comparePassword(password, (err, isMatch) => {
+            if (err) return done(err);
+            if (!isMatch) {
                 return done(null, false);
             }
             return done(null, user);
@@ -31,13 +31,33 @@ const localLogin = new LocalStrategy(localOptions, async (email, password, done)
 //setup options for Jwt(jason web token) strategy
 //we need to tell out strategy where to look for the token
 const jwtOptions = {
-//tells jwt that whenever a request comes in, in needs to look in
-//the header for he property called authorizattion, 
-//if we want passport to handle it
+    //tells jwt that whenever a request comes in, in needs to look in
+    //the header for he property called authorizattion, 
+    //if we want passport to handle it
     jwtFromRequest: ExtractJwt.fromHeader('authorization'),
-//tells jwt what secret we used to encode the token so we decode it
-secretOrKey: config.secret
-
+    //tells jwt what secret we used to encode the token so we decode it
+    secretOrKey: config.secret
 }
 
+//We are going to get the payload argument from an incoming request
+//the payload argument is coming from the function we create in authRoutes
+//done is the function we call once we try to authenticate this user
+const jwtLogin = new JwtStrategy(jwtOptions, async (payload, done) => {
+    try {
+        const user = await User.findById(payload.sub);
+        if (user) {
+            done(null, user);
+        } else {
+            done(null, false);
+        }
+    } catch (e) {
+        done(e, false);
+    }
+})
+//this tells passport that we declared there strategies.
+//the localLogin says we have a strategy called "Local".
+//the jwtLogin says we have a strategy called "jwt".
+
+//when we say passport.authentication('jwt'), passport will look for a strategy called jwt.
 passport.use(localLogin);
+passport.use(jwtLogin);
