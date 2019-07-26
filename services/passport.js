95a2835116c2d809a9a55501c68d9b1a4aaf402a
Shantell Davis <shantell87@gmail.com>
2019-07-26 11:47:18 -0700

    configure local strategy

--- services/passport.js
+++ services/passport.js
@@ -1,2 +1,29 @@
 const passport = require('passport');
 const User = require('./../models/User');
+const config = require('./../config');
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const LocalStrategy = require('passport.local');
+
+
+//create local strategy (use a strategy for each third party login system (eg Facebook or Google))
+//by default when logging in using LocalStrategy, it is expecting a username and password
+const localOptions = { usernameField: 'email' };
+
+const localLogin = new LocalStrategy(localOptions, async (email, password, done) => {
+    try {
+        const user = await User.findOne({email});
+        if(!user){
+            return done(null, false);
+        }
+        user.comparePassword(password, (err, isMatch) =>{
+            if(err) return done(err);
+            if(!isMatch) {
+                return done(null, false);
+            }
+            return done(null, user);
+        })
+    } catch (e) {
+        done(e, false);
+    }
+})
