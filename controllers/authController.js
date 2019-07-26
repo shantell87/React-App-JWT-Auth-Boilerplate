9ad34f089be0e8f42d5ea8fe62e9b3218644242b
Shantell Davis <shantell87@gmail.com>
2019-07-26 14:24:52 -0700

    OAuth Template complete

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,16 +1,16 @@
-const db     = require('./../models');
-const jwt    = require('jwt-simple');
+const db = require('./../models');
+const jwt = require('jwt-simple');
 const config = require('./../config');
 
 //create a token for the user
-const tokenForUser = function(user) {
+const tokenForUser = function (user) {
     const timestamp = new Date().getTime();
     // Sub === subject
     // iat === issued at time
-  
+
     // Its going to encode the whole 1st object and then add our secret to it
-    return jwt.encode({ sub: user.id, iat: timestamp}, config.secret);
-  };
+    return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
+};
 
 module.exports = {
     signUp: async (req, res) => {
@@ -21,19 +21,19 @@ module.exports = {
         }
         try { //check for existing user. If exist, throw error
             const existingUser = await db.User.findOne({ email });
-            if(existingUser) {
-                return res.status(422).json({error: 'Email is in use'});
+            if (existingUser) {
+                return res.status(422).json({ error: 'Email is in use' });
             }
-            const user = new db.User({email, password});
+            const user = new db.User({ email, password });
             await user.save();
-            res.json({ token : tokenForUser(user)});
+            res.json({ token: tokenForUser(user) });
 
         } catch (error) {
-            res.status(404).json({error});
+            res.status(404).json({ error });
         }
     },
 
     signIn: (req, res) => {
-        res.send("I'm hit!");
+        res.send({token: tokenForUser(req.user)});
     }
 };
