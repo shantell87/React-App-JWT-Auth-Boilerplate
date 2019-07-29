dfd2aeceb45ffe598f1610dffef7d16410479fd0
Shantell Davis <shantell87@gmail.com>
2019-07-29 11:53:07 -0700

    create stuff container

---
+++ client/src/containers/Stuff.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+class Stuff extends Component {
+    state = {
+        myFaveNumber: 5
+    }
+    handleIncrement = () => {
+        this.setState({ myFaveNumber: this.state.myFaveNumber + 1 });
+    }
+
+    handleDecrement = () => {
+        this.setState({ myFaveNumber: this.state.myFaveNumber - 1 });
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>myFaveNumber: {this.state.myFaveNumber}</h1>
+                <button onClick={this.handleIncrement}>Up</button>
+                <button onClick={this.handleDecrement}>Down</button>
+            </div>
+        )
+    }
+}
+
+export default Stuff;
