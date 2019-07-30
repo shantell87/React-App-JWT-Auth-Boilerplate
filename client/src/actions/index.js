0caf9d17cb61ea788a1665b38a779db7f0120029
Shantell Davis <shantell87@gmail.com>
2019-07-30 10:59:05 -0700

    add form validation to signup page. create signin page and hook up signin action

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -24,3 +24,16 @@ export const signup = (formProps, callback) => async dispatch => {
             dispatch({type: AUTH_ERROR, payload:"Email in use"});
         }
     };
+
+    export const signin = (formProps, callback) => async dispatch => {
+        try {
+            const res = await axios.post('/api/auth/signin', formProps);
+            dispatch({ type: AUTH_USER, payload: res.data.token });
+            localStorage.setItem('token', res.data.token);
+            callback();
+        } catch (e) {
+            dispatch({type: AUTH_ERROR, payload:"Invalid login credentails"});
+        }
+    };
+
+
